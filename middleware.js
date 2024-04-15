094d6dbffd11f3a8a159a5a6afbf920f9652b480
GhostChaser <batsiashvili.george@gmail.com>
2024-04-15 17:24:43 +0400

    feat: updated

--- middleware.js
+++ middleware.js
@@ -2,10 +2,15 @@ const jwt = require("jsonwebtoken"); // Import jsonwebtoken package
 const { getUserByToken } = require('./database')
 
 async function verifyToken(req, res, next) {
-    const token = req.headers['authorization'];
-    const user = await getUserByToken(token);
-    req.user = user;
-    next();
+    try {
+      const token = req.headers['authorization'];
+      const user = await getUserByToken(token);
+      req.user = user;
+      next(); 
+    } catch (error) {
+      console.log('error:', error)
+      res.status(400);
+    }
   }
   
 
